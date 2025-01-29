9da1d3d325436ee58f3ffda1249dfffca825a4ef
Akash <159242428+Akash9t2@users.noreply.github.com>
2025-01-29 19:36:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 class Config:
     API_ID=22981583
     API_HASH="c02ca0834d5d92ffb31fea2b50060195"
-    TOKEN="7156025226:AAFaTvFJwfkHLGcP2JZqY6GbfnUfajkQY5o"
-    SUDO = list(int(i) for i in os.environ.get("SUDO", "6752444356").split(" "))
+    TOKEN="7699382411:AAFE6zaorqtitFS0mcayLsSKUKnQMBVXneI"
+    SUDO = list(int(i) for i in os.environ.get("SUDO", "7660786977").split(" "))
     START_IMG="https://te.legra.ph/file/735920504b44b3565242a.jpg"
-    BOT_ID=7156025226
-    BOT_USERNAME="SOMEONE47BOT"
-    BOT_NAME="SOMEONE47BOT"
+    BOT_ID=7699382411
+    BOT_USERNAME="Shivay_baby_bot"
+    BOT_NAME="Shivay baby"
